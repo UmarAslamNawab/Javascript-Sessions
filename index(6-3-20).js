39fc385e676fe683ced3226746159d390e88e44e
UmarAslamNawab <umar_uni321@outlook.com>
2020-03-09 17:57:16 +0500

    Javascript Session(6-3-20)

---
+++ index(6-3-20).js
@@ -0,0 +1,129 @@
+//=================Task 1 ========================
+
+// var a = 10;
+
+// document.write("<h2>" + "Result" + "</h2>");
+// document.write("The value of document a is " + a + "<br>");
+// document.write(
+//   "......................................................" + "<br>"
+// );
+
+// document.write("The value of  ++a is " + ++a + "<br>");
+
+//=================Task 2 ========================
+
+// var a = 2;
+// var b = 1;
+// var result = --a - --b + ++b + b--;
+//             //    1  -  0 +  1  + 1
+// console.log(a);
+// console.log(b);
+// console.log(result);
+
+//=================Task 3 ========================
+
+// var userInput = +prompt("Enter table number", 5);
+// var count = 0;
+// document.write(userInput + " X " + ++count + " = " + userInput * count + "<br>");
+// document.write(userInput + " X " + ++count + " = " + userInput * count + "<br>");
+// document.write(userInput + " X " + ++count + " = " + userInput * count + "<br>");
+// document.write(userInput + " X " + ++count + " = " + userInput * count + "<br>");
+// document.write(userInput + " X " + ++count + " = " + userInput * count + "<br>");
+// document.write(userInput + " X " + ++count + " = " + userInput * count + "<br>");
+// document.write(userInput + " X " + ++count + " = " + userInput * count + "<br>");
+// document.write(userInput + " X " + ++count + " = " + userInput * count + "<br>");
+// document.write(userInput + " X " + ++count + " = " + userInput * count + "<br>");
+// document.write(userInput + " X " + ++count + " = " + userInput * count + "<br>");
+
+//=================Task 4 ========================
+
+// document.write("Subject total marks obtained marks Percentage" + "<br>");
+// var totalMarksEach = 100;
+// var subject1 = prompt("Enter Subject 1 Name");
+// var subject2 = prompt("Enter Subject 2 Name");
+// var subject3 = prompt("Enter Subject 3 Name");
+
+// var subject1Marks = +prompt("Enter Your " + subject1 + " Marks");
+// var subject2Marks = +prompt("Enter Your " + subject2 + " Marks");
+// var subject3Marks = +prompt("Enter Your " + subject3 + " Marks");
+
+// document.write(subject1 +" " +totalMarksEach +" " +subject1Marks +" " +(totalMarksEach / 100) * subject1Marks+ "%" + "<br>");
+// document.write(subject2 +" " +totalMarksEach +" " +subject2Marks +" " +(totalMarksEach / 100) * subject2Marks+ "%" + "<br>");
+// document.write(subject3 +" " +totalMarksEach +" " +subject3Marks +" " +(totalMarksEach / 100) * subject3Marks+ "%" + "<br>");
+
+// //=================Task 5 ========================
+
+// var city = prompt("Enter City Name");
+
+// if (city === "karachi") {
+//   alert("“Welcome to city of lights”");
+// }
+
+// //=================Task 6 ========================
+
+// var color = prompt("Enter color of road traffic signal");
+
+// if (color === "red") {
+//   alert("Must Stop");
+// }
+
+// if (color === "yellow") {
+//   alert("Ready to move");
+// }
+
+// if (color === "green") {
+//   alert("Move now");
+// }
+
+// //=================Task 7 ========================
+
+// var remainingFuel = +prompt("Enter remaining fuel in car");
+
+// if (remainingFuel < 0.25) {
+//   alert("Please refill the fuel in your car");
+// }
+
+// //=================Task 8========================
+
+// var a = 4;
+
+// if (++a === 5) {
+//   alert("given condition for variable is true");
+// }
+
+// var b = 82;
+// if (b++ === 83) {
+//   alert("given condition for variable b is true");
+// }
+
+// var c = 12;
+// if (c++ === 13) {
+//   alert("condition 1 is true");
+// }
+// if (c === 13) {
+//   alert("condition 2 is true");
+// }
+// if (++c < 14) {
+//   alert("condition 3 is true");
+// }
+// if (c === 14) {
+//   alert("condition 4 is true");
+// }
+
+// if ("caq" < "cat") {
+//   alert("car is smaller than cat");
+// }
+
+// //=================Task 9========================
+
+// var num = +prompt("Enter Your Number");
+
+// if (num % 3 === 0) {
+//   alert("Correct");
+// }
+
+// if (num % 2 == 0) {
+//   alert("Even Number");
+// } else {
+//   alert("Odd Number");
+// }
